a66b4a067c6a634ada64adbbf534e7092b0869c8
Joost Baaij <joost@spacebabies.nl>
2016-06-28 20:07:26 +0200

    convert the global sharing scripts into widgets

--- app/assets/javascripts/scrollytelling/pageflow/navigation/sharing.js
+++ app/assets/javascripts/scrollytelling/pageflow/navigation/sharing.js
@@ -1,18 +1,30 @@
-$(document).on('click', '#share-fb', function(event) {
-  event.preventDefault();
-  var fbSharer = window.open(
-    "//www.facebook.com/sharer/sharer.php?u="+encodeURIComponent(location.href.replace(location.hash,"")),
-    "Facebook",
-    "width=600,height=300"
-  );
+$.widget('scrollytelling.facebookSharing', {
+  _create: function() {
+    $('#share-fb', this.element).on('click', this.onClick);
+  },
+
+  onClick: function(event) {
+    event.preventDefault();
+    var fbSharer = window.open(
+      "//www.facebook.com/sharer/sharer.php?u="+encodeURIComponent(location.href.replace(location.hash,"")),
+      "Facebook",
+      "width=600,height=300"
+    );
+  }
 });
 
-$(document).on('click', '#share-tw', function(event) {
-  event.preventDefault();
-  var summary = $("meta[name='description']").attr('content');
-  var twSharer = window.open(
-    "//twitter.com/intent/tweet?text="+encodeURIComponent(summary)+"&amp;via=Scrollytellio&amp;url="+encodeURIComponent(location.href.replace(location.hash,"")),
-    "Twitter",
-    "width=600,height=260"
-  )
+$.widget('scrollytelling.twitterSharing', {
+  _create: function() {
+    $('#share-tw', this.element).on('click', this.onClick);
+  },
+
+  onClick: function(event) {
+    event.preventDefault();
+    var summary = $("meta[name='description']").attr('content');
+    var twSharer = window.open(
+      "//twitter.com/intent/tweet?text="+encodeURIComponent(summary)+"&amp;via=Scrollytellio&amp;url="+encodeURIComponent(location.href.replace(location.hash,"")),
+      "Twitter",
+      "width=600,height=260"
+    )
+  }
 });
--- app/assets/javascripts/scrollytelling/pageflow/navigation/widget_type.js
+++ app/assets/javascripts/scrollytelling/pageflow/navigation/widget_type.js
@@ -2,5 +2,7 @@ pageflow.widgetTypes.register('scrollytelling_pageflow_navigation', {
   enhance: function(element) {
     element.scrollytellingNavigationCounter();
     element.scrollytellingNavigationInvert();
+    element.facebookSharing();
+    element.twitterSharing();
   }
 });
