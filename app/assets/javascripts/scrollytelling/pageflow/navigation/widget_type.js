b9e0488a14e0be6307fd3dd547586b0121da4100
Joost Baaij <joost@spacebabies.nl>
2016-07-29 21:55:47 +0200

    hide the sharing for URLs containing 'onsite'
    It's a little hacky but I didn't want to pollute the editor with an
    option for this yet. So now whenever a story URL contains 'onsite' it
    will hide the sharing buttons.
    
    If this becomes a regularly used feature it would be better to
    officially add this as an option for the entry screen.

--- app/assets/javascripts/scrollytelling/pageflow/navigation/widget_type.js
+++ app/assets/javascripts/scrollytelling/pageflow/navigation/widget_type.js
@@ -5,5 +5,11 @@ pageflow.widgetTypes.register('scrollytelling_pageflow_navigation', {
     element.facebookSharing();
     element.twitterSharing();
     element.audioControl();
+
+    // hacky: if the entry URL matches 'onsite', push onsite class on the navigation.
+    // this allows us to hide the sharing widgets for onsite productions.
+    if($('head link[href*="onsite"]')) {
+      element.addClass('onsite');
+    }
   }
 });
