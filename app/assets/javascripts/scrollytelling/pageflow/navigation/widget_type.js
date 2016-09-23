9e2cffc9a428eafb4d3a0d6f5b216a62678f3fe1
Joost Baaij <joost@spacebabies.nl>
2016-09-23 12:50:09 +0200

    completely hide bak button in mobile
    and enable it globally.

--- app/assets/javascripts/scrollytelling/pageflow/navigation/widget_type.js
+++ app/assets/javascripts/scrollytelling/pageflow/navigation/widget_type.js
@@ -6,6 +6,11 @@ pageflow.widgetTypes.register('scrollytelling_pageflow_navigation', {
     element.twitterSharing();
     element.audioControl();
 
+    // Enable the parent page globally (Pageflow enables it only in mobile nav).
+    $('.parent_page', element).parentPageButton({
+      visibleClass: 'is_visible'
+    })
+
     // hacky: if the entry URL matches 'onsite', push onsite class on the navigation.
     // this allows us to hide the sharing widgets for onsite productions.
     if($('head link[href*="onsite"]').length > 0) {
--- lib/scrollytelling/pageflow/navigation/version.rb
+++ lib/scrollytelling/pageflow/navigation/version.rb
@@ -1,7 +1,7 @@
 module Scrollytelling
   module Pageflow
     module Navigation
-      VERSION = "1.7.0"
+      VERSION = "1.8.0"
     end
   end
 end
