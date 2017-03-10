2a5779bec7667ef3af4e201a030053223dd831c1
Vincent Kranendonk <vincent@studiorabota.com>
2017-03-10 17:58:21 +0100

    Show dots per chapter

--- app/assets/javascripts/scrollytelling/pageflow/navigation/counter.js
+++ app/assets/javascripts/scrollytelling/pageflow/navigation/counter.js
@@ -1,19 +1,28 @@
 $.widget('scrollytelling.scrollytellingNavigationCounter', {
+  // Active
   setActive: function(page) {
     this.element.find('a').removeClass('active');
     this.element.find('a[href="#' + page.attr('id') + '"]').addClass('active');
   },
 
-  updateActive: function(page) {
-    this.setActive(page.element);
+  // Show dots per chapter
+  showDotsForThisChapter: function(page) {
+    // hide others
+    this.element.find('li').removeClass('currentChapter');
+
+    // show only dots for this chapter
+    this.element.find('li[data-page-chapter-id="' + page[0].dataset.chapterId + '"]').addClass('currentChapter');
+  },
+
+  // Page change
+  pageChange: function(page) {
+        this.setActive(page.element);
+        this.showDotsForThisChapter(page.element);
   },
 
   _create: function() {
-    // Init active dot
-    this.setActive(pageflow.slides.currentPage());
-
     // Listen to page changes
-    pageflow.events.on('page:change', this.updateActive.bind(this));
+    pageflow.events.on('page:change', this.pageChange.bind(this));
 
     // Set click handler
     $('a', this.element).on('click', this.onClick);
