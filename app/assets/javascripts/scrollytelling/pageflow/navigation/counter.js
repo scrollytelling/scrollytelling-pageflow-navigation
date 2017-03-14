a63b3537f72a156053392734c6e940e090381030
Joost Baaij <joost@spacebabies.nl>
2017-03-14 13:14:10 +0100

    Revert "Show dots per chapter (#3)"
    Now the dots only show up when you have chapters. Most stories do not
    have chapters.
    
    Reverting from master until we can fix it.
    
    This reverts commit 5f6d2677ecd6085454ae4bedbb2d75465edb9962.

--- app/assets/javascripts/scrollytelling/pageflow/navigation/counter.js
+++ app/assets/javascripts/scrollytelling/pageflow/navigation/counter.js
@@ -1,28 +1,19 @@
 $.widget('scrollytelling.scrollytellingNavigationCounter', {
-  // Active
   setActive: function(page) {
     this.element.find('a').removeClass('active');
     this.element.find('a[href="#' + page.attr('id') + '"]').addClass('active');
   },
 
-  // Show dots per chapter
-  showDotsForThisChapter: function(page) {
-    // hide others
-    this.element.find('li').removeClass('currentChapter');
-
-    // show only dots for this chapter
-    this.element.find('li[data-page-chapter-id="' + page[0].dataset.chapterId + '"]').addClass('currentChapter');
-  },
-
-  // Page change
-  pageChange: function(page) {
-        this.setActive(page.element);
-        this.showDotsForThisChapter(page.element);
+  updateActive: function(page) {
+    this.setActive(page.element);
   },
 
   _create: function() {
+    // Init active dot
+    this.setActive(pageflow.slides.currentPage());
+
     // Listen to page changes
-    pageflow.events.on('page:change', this.pageChange.bind(this));
+    pageflow.events.on('page:change', this.updateActive.bind(this));
 
     // Set click handler
     $('a', this.element).on('click', this.onClick);
